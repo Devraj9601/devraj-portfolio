5b369b5ff2e8e202f28b39aa6034b1b178d4ed35
Ponnitivalasa Devraj <devrajp994@gmail.com>
2026-07-01 22:04:48 +0530

    commit

---
+++ dist/server/assets/_tanstack-start-manifest_v-Dbc99bAa.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "E:/portfolio-source/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-ww1fV1_P.js"] } }, clientEntry: "/assets/index-ww1fV1_P.js" });
+export {
+  tsrStartManifest
+};
